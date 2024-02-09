d0106f2a9059634cffcb2d55ae6e0ddb58b512f7
Am1d1v <dimadiman1adima@gmail.com>
2024-02-09 15:43:18 +0300

    Show data in DOM

--- app.js
+++ app.js
@@ -7,12 +7,23 @@ const output = document.querySelector('.output')
 
 
 // Event Lsiteners
-//
+// Fetch People's Data
 btn.addEventListener('click', async(event) => {
     const localData = await fetch(localUrl);
     const data = await localData.json();
-    console.log(data);
+
+    // Show Data in DOM
+    ShowInDOM(data);
 });
+
+//  Show Data in DOM
+function ShowInDOM(data){
+    output.innerHTML = '<h2>JSON Data</h2>';
+
+    data.forEach((element) => {
+        output.innerHTML += `<p>${element.name.first} ${element.name.last}</p>`;
+    });
+}
 
 
 
